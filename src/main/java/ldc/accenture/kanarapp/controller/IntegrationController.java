7d4e116dc59557d60ee07c9ce6ccbf6fa0b88ab6
Michal <michal@bajdek.pl>
2017-09-24 20:27:20 +0200

    nowy rm

--- src/main/java/ldc/accenture/kanarapp/controller/IntegrationController.java
+++ src/main/java/ldc/accenture/kanarapp/controller/IntegrationController.java
@@ -1,11 +1,14 @@
 package ldc.accenture.kanarapp.controller;
 
+import ldc.accenture.kanarapp.model.NotificationEvent;
 import ldc.accenture.kanarapp.util.RequestUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -46,5 +49,21 @@ public class IntegrationController {
     public String getAuthenticationDetails(HttpServletResponse response){
         log.error("response: " + response.toString());
         return "welcome";
+    }
+
+    @RequestMapping(value="/notification", method= RequestMethod.GET)
+    public ModelAndView getNewNotification(@RequestParam(value="tram_nr", defaultValue="1") String tram_nr,
+                                           @RequestParam(value="comment", defaultValue="") String comment,
+                                           @RequestParam(value="longitude", defaultValue = "0") String longitude,
+                                           @RequestParam(value="latitude",defaultValue = "0") String latitude){
+        ModelAndView mov = new ModelAndView("callback");
+        NotificationEvent notificationEvent = new NotificationEvent();
+        notificationEvent.setTramNr__c(tram_nr);
+        notificationEvent.setComment__c(comment);
+        notificationEvent.setLongitude__c(longitude);
+        notificationEvent.setLatitude__c(latitude);
+
+        log.info(notificationEvent.toString());
+        return mov;
     }
 }
