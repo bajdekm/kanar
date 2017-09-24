a4ad8e4e0d925e81333d06fcd866a7753d36c2d1
Michal <michal@bajdek.pl>
2017-09-24 20:47:46 +0200

    fix endpoint

--- src/main/java/ldc/accenture/kanarapp/constans/Constants.java
+++ src/main/java/ldc/accenture/kanarapp/constans/Constants.java
@@ -3,7 +3,7 @@ package ldc.accenture.kanarapp.constans;
 public class Constants {
 
     public static final String TOKEN_ENDPOINT = "https://login.salesforce.com/services/oauth2/token";
-    public static final String INSERT_OBJECT_ENDPOINT = "https://emibe-dev-ed.my.salesforce.com/services/data/v40.0/sobjects/Notification__e/";
+    public static final String INSERT_OBJECT_ENDPOINT = "https://emibe-dev-ed.my.salesforce.com/services/data/v40.0/sobjects/Alert__e/";
     public static final String CALLBACK_ENDPOINT = "https://kanardefender.herokuapp.com/i/handshake";
     public static final String CLIENT_ID = "3MVG9HxRZv05HarQtjSTFTJtOkxdwJakZL1ryiv8qhpFdlUyChqtdt2kbda12lsGWhH20YJ446wJUDs1f8J33";
     public static final String CLIENT_SECRET = "5145465745540914530";
